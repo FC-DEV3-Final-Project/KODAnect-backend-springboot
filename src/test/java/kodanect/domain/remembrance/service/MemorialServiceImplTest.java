6626c56581127663987c56f133e93bbef1e4af18
Choi-hoe-jin <gkwksk3@gmail.com>
2025-06-17 19:05:51 +0900

    refactor: 테스트 메서드에서 불필요한 throws Exception 제거
    테스트 메서드 내에서 실제로 Exception이 발생하지 않으므로 throws Exception 선언 제거
    - 코드 가독성 및 정적 분석 도구 경고 해소
    - JUnit 테스트 구조에 맞게 불필요한 예외 선언 정리

--- src/test/java/kodanect/domain/remembrance/service/MemorialServiceImplTest.java
+++ src/test/java/kodanect/domain/remembrance/service/MemorialServiceImplTest.java
@@ -53,7 +53,7 @@ public class MemorialServiceImplTest {
 
     @Test
     @DisplayName("추모관 이모지 카운팅")
-    public void 추모관_이모지_카운팅() throws Exception {
+    public void 추모관_이모지_카운팅() {
         /* emotionCountUpdate */
 
         Integer donateSeq = 1;
@@ -121,7 +121,7 @@ public class MemorialServiceImplTest {
 
     @Test
     @DisplayName("추모관 게시글 검색 조회")
-    public void 추모관_게시글_검색_조회() throws Exception {
+    public void 추모관_게시글_검색_조회() {
         /* getSearchMemorialList */
         MemorialNextCursor nextCursor = new MemorialNextCursor(null, null);
         int size = 20;
@@ -163,7 +163,7 @@ public class MemorialServiceImplTest {
 
     @Test
     @DisplayName("추모관 게시글 하늘나라 팝업 검색 조회")
-    public void 추모관_게시글_하늘나라_팝업_검색_조회() throws Exception {
+    public void 추모관_게시글_하늘나라_팝업_검색_조회() {
         /* getSearchMemorialList */
         Integer page = 1;
         int size = 20;
@@ -206,7 +206,7 @@ public class MemorialServiceImplTest {
 
     @Test
     @DisplayName("추모관 게시글 리스트 조회")
-    public void 추모관_게시글_리스트_조회() throws Exception {
+    public void 추모관_게시글_리스트_조회() {
         /* getMemorialList */
 
         MemorialNextCursor nextCursor = new MemorialNextCursor(null, null);
@@ -244,7 +244,7 @@ public class MemorialServiceImplTest {
 
     @Test
     @DisplayName("추모관 게시글 상세 조회")
-    public void 추모관_게시글_상세_조회() throws Exception {
+    public void 추모관_게시글_상세_조회() {
         /* getMemorialByDonateSeq */
 
         Integer donateSeq = 1;
