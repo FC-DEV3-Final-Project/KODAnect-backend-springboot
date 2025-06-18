825a8f20be4b84455dd9888b3f7c9dd54ab2aaeb
Choi-hoe-jin <gkwksk3@gmail.com>
2025-06-18 15:33:32 +0900

    refactor: HTML 템플릿 단일 문자열 리터럴로 변경
    단일 라인 HTML 템플릿에 대해 unnecessary text block ("""") 제거\n정적 분석 도구 경고 해결 및 코드 가독성 개선

--- src/main/java/kodanect/common/util/MemorialHtmlNormalizer.java
+++ src/main/java/kodanect/common/util/MemorialHtmlNormalizer.java
@@ -10,8 +10,9 @@ import kodanect.domain.remembrance.exception.InvalidContentsException;
  * */
 public class MemorialHtmlNormalizer {
 
-    private static final String TEMPLATE = """
-            <p>기증자 %s (%s, %s) 님은 %s 환자들에게 귀중한 장기를 선물해 주셨습니다.</p><p>한국장기조직기증원은 귀한 생명을 나눠주신 기증자와 유가족께 깊이 감사드리며,<br />앞으로도 기증자 유가족들이 건강한 삶을 유지할 수 있도록 최선을 다해 지원할 것입니다.</p><p>고인의 명복을 빕니다.</p>""";
+    private static final String TEMPLATE = "<p>기증자 %s (%s, %s) 님은 %s 환자들에게 귀중한 장기를 선물해 주셨습니다.</p>" +
+            "<p>한국장기조직기증원은 귀한 생명을 나눠주신 기증자와 유가족께 깊이 감사드리며,<br />앞으로도 기증자 유가족들이 건강한 삶을 유지할 수 있도록 최선을 다해 지원할 것입니다.</p>" +
+            "<p>고인의 명복을 빕니다.</p>";
 
     private MemorialHtmlNormalizer() {
         throw new UnsupportedOperationException("Utility class");
